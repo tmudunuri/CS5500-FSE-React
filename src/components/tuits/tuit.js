124efa25dbb36e8cf5786b09fb13b4bb76883e2b
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2022-03-27 15:21:08 -0400

    Align tuit component code

--- src/components/tuits/tuit.js
+++ src/components/tuits/tuit.js
@@ -13,53 +13,53 @@ const Tuit = ({tuit, deleteTuit, likeTuit}) => {
         const postedMillis = posted.getTime();
         const oldMillis = nowMillis - postedMillis;
         let old = 0.0;
-        const secondsOld = oldMillis/1000.0;
-        const minutesOld = secondsOld/60.0;
-        const hoursOld = minutesOld/60.0;
-        const daysOld = hoursOld/24.0;
-        if(daysOld > 1) {
+        const secondsOld = oldMillis / 1000.0;
+        const minutesOld = secondsOld / 60.0;
+        const hoursOld = minutesOld / 60.0;
+        const daysOld = hoursOld / 24.0;
+        if (daysOld > 1) {
             old = Math.round(daysOld) + 'd';
-        } else if(hoursOld > 1) {
+        } else if (hoursOld > 1) {
             old = Math.round(hoursOld) + 'h';
-        } else if(minutesOld > 1) {
+        } else if (minutesOld > 1) {
             old = Math.round(minutesOld) + 'm';
-        } else if(secondsOld > 1) {
+        } else if (secondsOld > 1) {
             old = Math.round(secondsOld) + 's';
         }
         return old;
     }
-  return(
-    // <li onClick={() => navigate(`/tuit/${tuit._id}`)}
-    <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
-      <div className="pe-2">
-        {
-          tuit.postedBy &&
-          <img src={`../images/${tuit.postedBy.username}.jpg`}
-               className="ttr-tuit-avatar-logo rounded-circle"/>
-        }
-      </div>
-      <div className="w-100">
-          <i onClick={() => deleteTuit(tuit._id)} className="fas fa-remove fa-2x fa-pull-right"></i>
-          <Link to={`/tuit/${tuit._id}`}>
-          <i className="float-end fas fa-circle-ellipsis me-1"></i>
-          </Link>
-        <h2
-          className="fs-5">
-          {tuit.postedBy && tuit.postedBy.username}
-          @{tuit.postedBy && tuit.postedBy.username} -
-            <span className="ms-1">{daysOld(tuit)}</span></h2>
-        {tuit.tuit}
-        {
-          tuit.youtube &&
-            <TuitVideo tuit={tuit}/>
-        }
-        {
-          tuit.image &&
-          <TuitImage tuit={tuit}/>
-        }
-        <TuitStats tuit={tuit} likeTuit={likeTuit}/>
-      </div>
-    </li>
-  );
+    return (
+        // <li onClick={() => navigate(`/tuit/${tuit._id}`)}
+        <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
+            <div className="pe-2">
+                {
+                    tuit.postedBy &&
+                    <img src={`../images/${tuit.postedBy.username}.jpg`}
+                         className="ttr-tuit-avatar-logo rounded-circle"/>
+                }
+            </div>
+            <div className="w-100">
+                <i onClick={() => deleteTuit(tuit._id)} className="fas fa-remove fa-2x fa-pull-right"></i>
+                <Link to={`/tuit/${tuit._id}`}>
+                    <i className="float-end fas fa-circle-ellipsis me-1"></i>
+                </Link>
+                <h2
+                    className="fs-5">
+                    {tuit.postedBy && tuit.postedBy.username}
+                    @{tuit.postedBy && tuit.postedBy.username} -
+                    <span className="ms-1">{daysOld(tuit)}</span></h2>
+                {tuit.tuit}
+                {
+                    tuit.youtube &&
+                    <TuitVideo tuit={tuit}/>
+                }
+                {
+                    tuit.image &&
+                    <TuitImage tuit={tuit}/>
+                }
+                <TuitStats tuit={tuit} likeTuit={likeTuit}/>
+            </div>
+        </li>
+    );
 }
 export default Tuit;
