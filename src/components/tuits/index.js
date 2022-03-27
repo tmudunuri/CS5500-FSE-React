c000db79ba7d5100fc777433796fcd9d0ff286fb
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2022-03-27 15:20:20 -0400

    Add deleteTuit param to Tuits component

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -2,29 +2,28 @@ import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
 import * as likesService from "../../services/likes-service";
-import * as service from "../../services/tuits-service";
-const Tuits = ({tuits = [], refreshTuits}) => {
+
+const Tuits = ({
+                   tuits = [], deleteTuit,
+                   refreshTuits
+               }) => {
     const likeTuit = (tuit) =>
         likesService.userLikesTuit("me", tuit._id)
             .then(refreshTuits)
             .catch(e => alert(e))
-    const deleteTuit = (tid) =>
-        service.deleteTuit(tid)
-            .then(refreshTuits);
-
     return (
         <div>
-          <ul className="ttr-tuits list-group">
-            {
-              tuits.map && tuits.map(tuit =>
-                  <Tuit key={tuit._id}
-                        deleteTuit={deleteTuit}
-                        likeTuit={likeTuit}
-                        tuit={tuit}/>)
-            }
-          </ul>
+            <ul className="ttr-tuits list-group">
+                {
+                    tuits.map && tuits.map(tuit =>
+                        <Tuit key={tuit._id}
+                              deleteTuit={deleteTuit}
+                              likeTuit={likeTuit}
+                              tuit={tuit}/>)
+                }
+            </ul>
         </div>
-      );
+    );
 }
 
 export default Tuits;
