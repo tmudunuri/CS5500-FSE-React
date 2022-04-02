90a2f75f469bc1d1d3f38e1378f5f1b9d5e6b46f
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2022-04-02 17:14:37 -0400

    Provide host options for tests

--- src/tests/tuit-list.test.js
+++ src/tests/tuit-list.test.js
@@ -5,9 +5,10 @@ import {findAllTuits} from "../services/tuits-service";
 import axios from "axios";
 
 const MOCKED_TUITS = [
-  {tuit: "alice's tuit", _id: "1"},
-  {tuit: "bob's tuit", _id: "2"},
-  {tuit: "charlie's tuit", _id: "3"}
+    {tuit: "aaa", _id: "1"},
+    {tuit: "alice's tuit", _id: "2"},
+    {tuit: "bob's tuit", _id: "3"},
+    {tuit: "charlie's tuit", _id: "4"}
 ];
 
 describe('static renders', () => {
@@ -23,7 +24,6 @@ describe('static renders', () => {
 
 describe('async renders', () => {
   test('tuit list renders async', async () => {
-    // TODO: implement this
     const allTuits = await findAllTuits();
     render(
         <HashRouter>
@@ -35,20 +35,19 @@ describe('async renders', () => {
 });
 
 describe('renders mocked', () => {
-  test('tuit list renders mocked', async () => {
-    // TODO: implement this
-    const mock = jest.spyOn(axios, 'get');
-    mock.mockImplementationOnce(() =>
-        Promise.resolve({data: {tuits: MOCKED_TUITS}}));
-    const response = await findAllTuits();
-    const mockedTuits = response.tuits;
-    mock.mockRestore();
+    test('tuit list renders mocked', async () => {
+        const mock = jest.spyOn(axios, 'get');
+        mock.mockImplementationOnce(() =>
+            Promise.resolve({data: {tuits: MOCKED_TUITS}}));
+        const response = await findAllTuits();
+        const mockedTuits = response;
+        mock.mockRestore();
 
-    render(
-        <HashRouter>
-          <Tuits tuits={mockedTuits}/>
-        </HashRouter>);
-    const linkElement = screen.getByText(/alice's tuit/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+        render(
+            <HashRouter>
+                <Tuits tuits={mockedTuits}/>
+            </HashRouter>);
+        const linkElement = screen.getByText(/aaa/i);
+        expect(linkElement).toBeInTheDocument();
+    });
 });
